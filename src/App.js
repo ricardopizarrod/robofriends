32163ecf45efb26f6fcd7bac0614d1358abc1814
Ricardo Pizarro <ricardopizarro@MacBook-Pro.local>
2020-01-22 19:34:55 -0300

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -38,10 +38,11 @@ export default App;   */
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots'
+import scroll from './Scroll';
+import { robots } from './robots';
 
 class App extends Component{
-	constructor() {
+	constructor() { 
 		super()
 		this.state = {
 			robots: robots,
@@ -49,25 +50,38 @@ class App extends Component{
 		}
 	}
 
+    componentDiMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(Response => Response.json())
+        .then(users => this.setState({ robots: users}));
+    }
+
 	onSearchChange = (event) => {
 		this.setState({ searchfield: event.target.value })
 		
 	}
 
 	render () {
+        const { robots, searchfield } = this.state;
 		const filteredRobots = this.state.robots.filter( robots => {
 			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-		})
-		return (
-		<div className='tc'>
-			<h1>RoboFiends</h1>
-			<SearchBox searchChange={this.onSearchChange} />
-			<CardList robots={filteredRobots}/>
-		</div>
-		);
-	}
+        })
+            return !robots.length?
+                <h1>Loading</h1> :
+            (
+                <div className='tc'>
+                    <h1>RoboFiends</h1>
+                 <SearchBox searchChange={this.onSearchChange} />
+                    <scroll>
+                    <   CardList robots={filteredRobots}/>
+                    </scroll>
+                </div>
+            );    
+		
+    }
+}
 	
 
-}
+
 
 export default App;
