00d3dc97a6a3e97040f48fb0003cbf4544718f62
Ricardo Pizarro <ricardopizarro@MacBook-Pro.local>
2020-01-22 21:01:28 -0300

    Updates

---
+++ precache-manifest.64f94622fe4f5d1e35274e34527d315f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4567b4bab5beb765dc1c6ecd05b50909",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "4ea886189846f3c75328",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "0807eacb401f396eb743",
+    "url": "/robofriends/static/css/main.b6b09676.chunk.css"
+  },
+  {
+    "revision": "4ea886189846f3c75328",
+    "url": "/robofriends/static/js/2.37296ba2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.37296ba2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0807eacb401f396eb743",
+    "url": "/robofriends/static/js/main.1f0050a2.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  }
+]);
